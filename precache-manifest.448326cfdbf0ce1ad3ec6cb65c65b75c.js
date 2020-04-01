32b5ffcec0af73ed46bf721f78621bffc38301ab
yuriy82791 <yuriy82791@gmail.com>
2020-04-01 18:58:21 +0300

    Updates

---
+++ precache-manifest.448326cfdbf0ce1ad3ec6cb65c65b75c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c999207e6c0c8f88a0771a8cc68ed6a",
+    "url": "/contacts-app/index.html"
+  },
+  {
+    "revision": "81677cdb3502aff9eb90",
+    "url": "/contacts-app/static/css/main.f9d39f95.chunk.css"
+  },
+  {
+    "revision": "1dd265370793914d9d25",
+    "url": "/contacts-app/static/js/2.1f6a1ce9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contacts-app/static/js/2.1f6a1ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81677cdb3502aff9eb90",
+    "url": "/contacts-app/static/js/main.5d83f1b1.chunk.js"
+  },
+  {
+    "revision": "edb454b9946441f55fd6",
+    "url": "/contacts-app/static/js/runtime-main.3c2c3a7b.js"
+  }
+]);
